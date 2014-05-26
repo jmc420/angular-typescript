77e6b3016a68a88b4bca23eeec61bd432258ab08
James Cowan <jamesmcowan@googlemail.com>
2014-05-26 13:28:39 +0100

    add source

---
+++ src/AngularTypescript/Application.ts
@@ -0,0 +1,17 @@
+///<reference path='../lib/DefinitelyTyped/angularjs/angular.d.ts'/>
+///<reference path='../lib/DefinitelyTyped/jquery/jquery.d.ts'/>
+
+module AngularTypescript {
+    export class Application {
+        constructor() {
+
+        }
+    }
+
+    $(document).ready(function () {
+        var module = angular.module("angularTypeScriptExampleApp", []);
+
+    });
+}
+
+/*******************************************************************/
